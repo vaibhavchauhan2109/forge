69d73878039e05822a3313c7865e260db91c3226
Vaibhav Singh Chauhan <77773076+vaibhavchauhan2109@users.noreply.github.com>
2026-08-03 14:22:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
       and you will lose an hour wondering why nothing updated.
    ============================================================ */
 
-const VERSION = 'v21';
+const VERSION = 'v22';
 const CACHE   = `forge-${VERSION}`;
 
 const ASSETS = [
